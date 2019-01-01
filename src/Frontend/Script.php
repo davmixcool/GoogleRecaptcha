8d6b31a7db554e3eaf0dcd1928161b2369857b7d
peter279k <peter279k@gmail.com>
2019-01-01 15:08:03 +0800

    Enhance source coding style

--- src/Frontend/Script.php
+++ src/Frontend/Script.php
@@ -10,15 +10,15 @@ class Script
 
 	protected $theme;
 
-	function __construct($options=array())
-	{	
+	public function __construct($options=[])
+	{
 		$this->site_key = isset($options['site_key'])? $options['site_key'] : null;
 		$this->selector = isset($options['selector'])? $options['selector'] : 'google_recaptcha_selector';
 		$this->theme = isset($options['theme'])? $options['theme'] : 'light';
 	}
 
 	public function render()
-	{	
+	{
 		return '
 			<script type="text/javascript">
 		      var onloadGoogleRecaptchaCallback = function() {
@@ -34,6 +34,3 @@ class Script
 		';
 	}
 }
-
-
-?>
--- src/GoogleRecaptcha.php
+++ src/GoogleRecaptcha.php
@@ -5,8 +5,7 @@ use Davmixcool\Frontend\Input;
 use Davmixcool\Frontend\Script;
 
 class GoogleRecaptcha
-{	
-
+{
 	protected $secret_key;
 
 	protected $verify_peer;
@@ -16,10 +15,10 @@ class GoogleRecaptcha
 	protected $options;
 
 	protected $script;
-	
+
 	protected $input;
 
-	function __construct($options=array())
+	public function __construct($options=[])
 	{
 		$this->secret_key = isset($options['secret_key'])? $options['secret_key'] : null;
 		$this->verify_peer = isset($options['verify_peer'])? $options['verify_peer'] : false;
@@ -31,77 +30,66 @@ class GoogleRecaptcha
 		$this->input = new Input($this->options);
 	}
 
-
 	protected function buildQuery($captcha=null)
 	{
 		return http_build_query(
-	        array(
+	        [
 	          'secret' => $this->secret_key, //secret KEy provided by google
 	          'response' => $captcha,   // g-captcha-response string sent from client
 	          'remoteip' => $_SERVER['REMOTE_ADDR']
-	        )
+			]
 	      );
 	}
-
 
 	protected function buildOptions($captcha=null)
 	{
 		//Build post data to make request with fetch_file_contents
      	$data = $this->buildQuery($captcha);
      	//Build options for the post request
-     	$options = array('http' =>
-	        array(
+     	$options = ['http' =>
+	        [
 	          'method'  => 'POST',
 	          'header'  => 'Content-type: application/x-www-form-urlencoded',
 	          'content' => $data
-	        ),
-	        "ssl"=>array(
+			],
+	        "ssl" => [
 	        "verify_peer"=> $this->verify_peer,
 	        "verify_peer_name"=> $this->verify_peer_name,
-	        ),
-	    );
+			],
+		];
+
 	    return $options;
 	}
-
 
 	protected function verifySite($context=null)
 	{
 		/* Send request to Googles siteVerify API */
-	    $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify",false,$context);
+	    $response = file_get_contents("https://www.google.com/recaptcha/api/siteverify", false, $context);
 	    $response = json_decode($response, true);
-    
-    	if($response["success"]===false) { //if user verification failed
-        	//Robots Not allowed (Captcha verification failed;
-        	return false;
-	    } else {
-       		return true;
-    	}
 
+		return $response["success"] === false;
 	}
 
-
 	public function check()
-	{	
-		$captcha = $_POST['g-recaptcha-response'];
+	{
+		if (!empty( $_POST['g-recaptcha-response'])) {
+			$captcha = $_POST['g-recaptcha-response'];
 
-		if (!empty($captcha)) {
-  
 	      	//Build options for the post request
 	      	$opts = $this->buildOptions($captcha);
 		    //Create a stream this is required to make post request with fetch_file_contents
-		    $context = stream_context_create($opts); 
+		    $context = stream_context_create($opts);
 
 		    return $this->verifySite($context);
 		}
+
 		return false;
 	}
-
 
 	public function renderJs()
 	{
 		return $this->script->render();
 	}
-
 
 	public function renderCaptcha()
 	{
@@ -109,5 +97,3 @@ class GoogleRecaptcha
 	}
 
 }
-
-?>
